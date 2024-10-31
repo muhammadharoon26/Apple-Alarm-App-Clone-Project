71a8c2edc22404ae00998002278ea37c62a5a2a0
Muhammad Haroon <mharoon1326@gmail.com>
2024-10-31 23:12:05 +0500

    init

---
+++ apple_clone_alarm_project/android/app/src/main/kotlin/com/example/apple_clone_alarm_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.apple_clone_alarm_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ apple_clone_alarm_project/lib/worldClock.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WorldClock());
+}
+class WorldClock extends StatefulWidget {
+  const WorldClock({super.key});
+
+  @override
+  State<WorldClock> createState() => WorldClockState();
+}
+
+class WorldClockState extends State<WorldClock> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
