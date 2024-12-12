703d122e18eded26f7b87876d24225335036387a
Muhammad Haroon <mharoon1326@gmail.com>
2024-12-12 15:35:00 +0500

    "Added ignore comment to library private types in public API warning in MyHomePage class."
    "Added ignore comment to library private types in public API warning in MyHomePage class."

--- apple_clone_alarm_project/lib/main.dart
+++ apple_clone_alarm_project/lib/main.dart
@@ -26,6 +26,7 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _MyHomePageState createState() => _MyHomePageState();
 }
 
